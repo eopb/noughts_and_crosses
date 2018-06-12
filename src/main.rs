f65fbc07cb2591db909f89defa8767c3bafa6b56
Ethan Brierley <ethanboxx@gmail.com>
2018-06-12 12:26:18 +0100

    work on data sturcture

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,29 @@ fn main() {
         Selected,
         Empty,
     }
+    struct row1 {
+        coloum1: PartStatus,
+        coloum2: PartStatus,
+        coloum3: PartStatus,
+    }
+    struct row2 {
+        coloum1: PartStatus,
+        coloum2: PartStatus,
+        coloum3: PartStatus,
+    
+    }
+    struct row3 {
+        coloum1: PartStatus,
+        coloum2: PartStatus,
+        coloum3: PartStatus,
+    
+}
+
+    struct Board {
+        row1: row1,
+        row2: row2,
+        row3: row3,
+    }
     draw_game_board();
 }
 
