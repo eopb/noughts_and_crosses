93fbcef652607eba7eec5162e5512240aa50e4bb
Ethan Brierley <ethanboxx@gmail.com>
2018-06-12 11:45:48 +0100

    testing mutation on the drawing of the board

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to my noughts and crosses game made in rust.");
     draw_game_board();
 }
 
 // Prints out the game board
 fn draw_game_board() {
-    println!("x|x|x");
+    let mut test = "x";
+    println!("{}|{}|{}", test, test ,test);
     println!("_ _ _");
-    println!("x|x|x");
+    println!("{}|{}|{}", test, test ,test);
     println!("_ _ _");
-    println!("x|x|x");
+    println!("{}|{}|{}", test, test ,test);
 }
