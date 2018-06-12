db1c7b3879b7c26de5957235228937c8e9877749
Ethan Brierley <ethanboxx@gmail.com>
2018-06-12 11:38:04 +0100

    game board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+    draw_game_board();
+}
+
+fn draw_game_board(){
+    println!("x|x|x");
+    println!("_ _ _");
+    println!("x|x|x");
+    println!("_ _ _");
+    println!("x|x|x");
 }
