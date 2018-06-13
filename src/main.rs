cd1da43f2bfab067ba3f1d26617a7d167210add1
Ethan Brierley <ethanboxx@gmail.com>
2018-06-13 14:10:34 +0100

    clean up stuff

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 fn main() {
     println!("Welcome to my noughts and crosses game made in rust.");
-    let row1 = [" ","x","0"];
-    let row2 = ["x","0"," "];
-    let row3 = ["0"," ","x"];
-
+    let mut row1 = [" "," "," "];
+    let mut row2 = [" "," "," "];
+    let mut row3 = [" "," "," "];
+    println!("Crosses goes first.");
 
     draw_game_board(&row1, &row2, &row3);
 }
 
 // Prints out the game board
-fn draw_game_board(row1: &[&str; 3],row2: &[&str; 3],row3: &[&str; 3]) {
+fn draw_game_board(
+    row1: &[&str; 3],
+    row2: &[&str; 3],
+    row3: &[&str; 3]
+    ) {
     println!("{}|{}|{}", row1[0], row1[1] ,row1[2]);
     println!("_ _ _");
     println!("{}|{}|{}", row2[0], row2[1] ,row2[2]);
