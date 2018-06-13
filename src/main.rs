05b194ea6cdc863d8ab52cb17147be06e105cf2c
Ethan Brierley <ethanboxx@gmail.com>
2018-06-13 18:18:09 +0100

    fail

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
     println!("Welcome to my noughts and crosses game made in rust.");
-    let mut row1 = [" "," "," "];
+    let mut row1 = ["x","x","x"];
     let mut row2 = [" "," "," "];
     let mut row3 = [" "," "," "];
     println!("Crosses goes first.");
 
     draw_game_board(&row1, &row2, &row3);
-    has_someone_won(&row1, &row2, &row3);
+    let winner = has_someone_won(&row1, &row2, &row3);
 }
 
 // Prints out the game board
@@ -26,40 +26,46 @@ fn has_someone_won(
     row1: &[&str; 3],
     row2: &[&str; 3],
     row3: &[&str; 3]
-    ) {
+    ) -> std::string::String {
     
-    let player = ["x", "0"];
+    let player = ["x", "0"] ;
+    let mut winner = "none".to_string();
     for &player in player.iter() {
         if (row1[0] == player) && (row1[1] == player) && (row1[2] == player){
-            println!("1")
+            mention_the_winner(player.to_string());
+            winner = player.to_string();
+            winner.to_string()
         }
         else if (row2[0] == player) && (row2[1] == player) && (row2[2] == player){
-            println!("2")
+            mention_the_winner(player.to_string());
         }
         else if (row3[0] == player) && (row3[1] == player) && (row3[2] == player){
-            println!("3")
+            mention_the_winner(player.to_string());
         }
 
 
 
         else if (row1[0] == player) && (row2[0] == player) && (row3[0] == player){
-            println!("4")
+            mention_the_winner(player.to_string());
         }
         else if (row1[1] == player) && (row2[1] == player) && (row3[1] == player){
-            println!("5")
+            mention_the_winner(player.to_string());
         }
         else if (row1[2] == player) && (row2[2] == player) && (row3[2] == player){
-            println!("6")
+            mention_the_winner(player.to_string());
         }
 
 
         else if (row1[0] == player) && (row2[1] == player) && (row3[2] == player){
-            println!("7")
+            mention_the_winner(player.to_string());
         }
         else if (row3[0] == player) && (row2[1] == player) && (row1[2] == player){
-            println!("8")
+            mention_the_winner(player.to_string());
         }
     }
-
+    fn mention_the_winner(winner: String) {
+        println!("The winner is {}", winner);
+    }
+    winner.to_string()
 }
 
