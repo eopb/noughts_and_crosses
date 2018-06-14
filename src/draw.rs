77fc217b5a132033fa2d846001ec5dd6c2aa7ebc
Ethan Brierley <ethanboxx@gmail.com>
2018-06-14 10:51:17 +0100

    game play

--- src/draw.rs
+++ src/draw.rs
@@ -3,11 +3,54 @@
 pub fn draw_game_board(
     row1: &[&str; 3],
     row2: &[&str; 3],
-    row3: &[&str; 3]
+    row3: &[&str; 3],
+    playerPosition: &[i32; 2]
     ) {
-    println!("{}|{}|{}", row1[0], row1[1] ,row1[2]);
+    let mut player11 = String::new();
+    let mut player12 = String::new();
+    let mut player13 = String::new();
+    let mut player21 = String::new();
+    let mut player22 = String::new();
+    let mut player23 = String::new();
+    let mut player31 = String::new();
+    let mut player32 = String::new();
+    let mut player33 = String::new();
+    if playerPosition[0] == 1 {
+        if playerPosition[1] == 1 {
+            player11 = "*".to_string();
+        }
+        else if playerPosition[1] == 2 {
+            player12 = "*".to_string();
+        }
+        else if playerPosition[1] == 3 {
+            player13 = "*".to_string();
+        }
+    }
+    else if playerPosition[0] == 2 {
+        if playerPosition[1] == 1 {
+            player21 = "*".to_string();
+        }
+        else if playerPosition[1] == 2 {
+            player22 = "*".to_string();
+        }
+        else if playerPosition[1] == 3 {
+            player23 = "*".to_string();
+        }
+    }
+    else if playerPosition[0] == 3 {
+        if playerPosition[1] == 1 {
+            player31 = "*".to_string();
+        }
+        else if playerPosition[1] == 2 {
+            player32 = "*".to_string();
+        }
+        else if playerPosition[1] == 3 {
+            player33 = "*".to_string();
+        }
+    }
+    println!("{}{}|{}{}|{}{}", row1[0], player11, row1[1], player12, row1[2], player13);
     println!("_ _ _");
-    println!("{}|{}|{}", row2[0], row2[1] ,row2[2]);
+    println!("{}{}|{}{}|{}{}", row2[0], player21, row2[1], player22, row2[2], player23);
     println!("_ _ _");
-    println!("{}|{}|{}", row3[0], row3[1] ,row3[2]);
+    println!("{}{}|{}{}|{}{}", row3[0], player31, row3[1], player32, row3[2], player33);
 }
--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,63 @@ fn main() {
     let mut row1 = [" "," "," "];
     let mut row2 = [" "," "," "];
     let mut row3 = [" "," "," "];
+
+    let mut player_position: [i32; 2] = [1,1];
+
     println!("Crosses goes first.");
 
     println!("The board looks like this.");
-    draw::draw_game_board(&row1, &row2, &row3);
+    draw::draw_game_board(&row1, &row2, &row3, &player_position);
     println!("You are the *");
-    println!("To move the star left type a and hit enter");
-    println!("To move the star right type d and hit enter");
-    println!("To move the star up type w and hit enter");
-    println!("To move the star down type s and hit enter");
-    println!("To place your cross type q and hit enter");
-    let mut guess = String::new();
+    loop {
+        println!("To move the star left type 4 and hit enter");
+        println!("To move the star right type 6 and hit enter");
+        println!("To move the star up type 8 and hit enter");
+        println!("To move the star down type 2 and hit enter");
+        println!("To place your cross type 0 and hit enter");
+        let mut movement = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin().read_line(&mut movement)
+            .expect("Failed to read line");
+        let movement: u32 = match movement.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
+        if movement == 4 {
+            if player_position[1] == 1 {
+                continue
+            }
+            player_position = [player_position[0] , player_position[1] - 1];
+            break;
+        }
+        else if movement == 6 {
+            if player_position[1] == 3 {
+                continue
+            }
+            player_position = [player_position[0] , player_position[1] + 1];
+            break;
+        }
+        else if movement == 8 {
+            if player_position[0] == 1 {
+                continue
+            }
+            player_position = [player_position[0] - 1 , player_position[1]];
+            break;
+        }
+        else if movement == 2 {
+            if player_position[0] == 3 {
+                continue
+            }
+            player_position = [player_position[0] + 1, player_position[1]];
+            break;
+        }
+        else if movement == 0 {
+            break;
+        }
+    }
+
+    draw::draw_game_board(&row1, &row2, &row3, &player_position);
     let winner = tests::has_someone_won(&row1, &row2, &row3);
     println!("winner {}", winner)
 }
