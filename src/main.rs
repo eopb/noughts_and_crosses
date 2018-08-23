afe82dfd6b658e668c7fb4d7090f42a74d6f61dd
Ethan Brierley <ethanboxx@gmail.com>
2018-08-23 22:10:53 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,164 +1,168 @@
 use std::io;
-mod draw;
-mod tests;
+// mod draw;
+// mod tests;
 
 fn main() {
-    println!("Welcome to my noughts and crosses game made in rust.");
-    let mut row1 = [" "," "," "];
-    let mut row2 = [" "," "," "];
-    let mut row3 = [" "," "," "];
+    println!("Hello World");
+}
 
-    let mut player_position: [i32; 2] = [1,1];
+// fn main() {
+//     println!("Welcome to my noughts and crosses game made in rust.");
+//     let mut row1 = [" "," "," "];
+//     let mut row2 = [" "," "," "];
+//     let mut row3 = [" "," "," "];
 
-    let mut current_player = "0";
+//     let mut player_position: [i32; 2] = [1,1];
 
-    println!("Crosses goes first.");
+//     let mut current_player = "0";
 
-    println!("The board looks like this.");
-    draw::draw_game_board(&row1, &row2, &row3, &player_position);
-    println!("You are the *");
-    loop {
-        if current_player == "0" {
-            current_player = "x";
-        }
-        else if current_player == "x" {
-            current_player = "0";
-        }
-        draw::draw_game_board(&row1, &row2, &row3, &player_position);
-        let winner = tests::has_someone_won(&row1, &row2, &row3);
-        println!("winner {}", winner);
-        loop {
-            println!("The player is {}", current_player);
-            println!("To move the star left type 4 and hit enter");
-            println!("To move the star right type 6 and hit enter");
-            println!("To move the star up type 8 and hit enter");
-            println!("To move the star down type 2 and hit enter");
-            println!("To place your cross type 5 and hit enter");
-            let mut movement = String::new();
+//     println!("Crosses goes first.");
 
-            io::stdin().read_line(&mut movement)
-                .expect("Failed to read line");
-            let movement: u32 = match movement.trim().parse() {
-                Ok(num) => num,
-                Err(_) => continue,
-            };
-            if (movement == 4) ^ (movement == 2) ^ (movement == 6) ^ (movement == 8) {
-                player_position = move_player(movement, player_position);
-            }
-            else if movement == 5 {
-                if player_position[0] == 1 {
-                    if player_position[1] == 1 {
-                        if row1[0] == " " {
-                            row1 = [current_player, row1[1], row1[2]];
-                            break;
-                        }
-                    }
-                    if player_position[1] == 2 {
-                        if row1[1] == " " {
-                            row1 = [row1[0], current_player, row1[2]];
-                            break;
-                        }
-                    }
-                    if player_position[1] == 3 {
-                        if row1[2] == " " {
-                            row1 = [row1[0], row1[1], current_player];
-                            break;
-                        }
-                    }
-                }
-                if player_position[0] == 2 {
-                    if player_position[1] == 1 {
-                        if row2[0] == " " {
-                            row2 = [current_player, row2[1], row2[2]];
-                            break;
-                        }
-                    }
-                    if player_position[1] == 2 {
-                        if row2[1] == " " {
-                            row2 = [row2[0], current_player, row2[2]];
-                            break;
-                        }
-                    }
-                    if player_position[1] == 3 {
-                        if row2[2] == " " {
-                            row2 = [row2[0], row2[1], current_player];
-                            break;
-                        }
-                    }
-                }
-                if player_position[0] == 3 {
-                    if player_position[1] == 1 {
-                        if row3[0] == " " {
-                            row3 = [current_player, row3[1], row3[2]];
-                            break;
-                        }
-                    }
-                    if player_position[1] == 2 {
-                        if row3[1] == " " {
-                            row3 = [row3[0], current_player, row3[2]];
-                            break;
-                        }
-                    }
-                    if player_position[1] == 3 {
-                        if row3[2] == " " {
-                            row3 = [row3[0], row3[1], current_player];
-                            break;
-                        }
-                    }
-                }
+//     println!("The board looks like this.");
+//     draw::draw_game_board(&row1, &row2, &row3, &player_position);
+//     println!("You are the *");
+//     loop {
+//         if current_player == "0" {
+//             current_player = "x";
+//         }
+//         else if current_player == "x" {
+//             current_player = "0";
+//         }
+//         draw::draw_game_board(&row1, &row2, &row3, &player_position);
+//         let winner = tests::has_someone_won(&row1, &row2, &row3);
+//         println!("winner {}", winner);
+//         loop {
+//             println!("The player is {}", current_player);
+//             println!("To move the star left type 4 and hit enter");
+//             println!("To move the star right type 6 and hit enter");
+//             println!("To move the star up type 8 and hit enter");
+//             println!("To move the star down type 2 and hit enter");
+//             println!("To place your cross type 5 and hit enter");
+//             let mut movement = String::new();
+
+//             io::stdin().read_line(&mut movement)
+//                 .expect("Failed to read line");
+//             let movement: u32 = match movement.trim().parse() {
+//                 Ok(num) => num,
+//                 Err(_) => continue,
+//             };
+//             if (movement == 4) ^ (movement == 2) ^ (movement == 6) ^ (movement == 8) {
+//                 player_position = move_player(movement, player_position);
+//             }
+//             else if movement == 5 {
+//                 if player_position[0] == 1 {
+//                     if player_position[1] == 1 {
+//                         if row1[0] == " " {
+//                             row1 = [current_player, row1[1], row1[2]];
+//                             break;
+//                         }
+//                     }
+//                     if player_position[1] == 2 {
+//                         if row1[1] == " " {
+//                             row1 = [row1[0], current_player, row1[2]];
+//                             break;
+//                         }
+//                     }
+//                     if player_position[1] == 3 {
+//                         if row1[2] == " " {
+//                             row1 = [row1[0], row1[1], current_player];
+//                             break;
+//                         }
+//                     }
+//                 }
+//                 if player_position[0] == 2 {
+//                     if player_position[1] == 1 {
+//                         if row2[0] == " " {
+//                             row2 = [current_player, row2[1], row2[2]];
+//                             break;
+//                         }
+//                     }
+//                     if player_position[1] == 2 {
+//                         if row2[1] == " " {
+//                             row2 = [row2[0], current_player, row2[2]];
+//                             break;
+//                         }
+//                     }
+//                     if player_position[1] == 3 {
+//                         if row2[2] == " " {
+//                             row2 = [row2[0], row2[1], current_player];
+//                             break;
+//                         }
+//                     }
+//                 }
+//                 if player_position[0] == 3 {
+//                     if player_position[1] == 1 {
+//                         if row3[0] == " " {
+//                             row3 = [current_player, row3[1], row3[2]];
+//                             break;
+//                         }
+//                     }
+//                     if player_position[1] == 2 {
+//                         if row3[1] == " " {
+//                             row3 = [row3[0], current_player, row3[2]];
+//                             break;
+//                         }
+//                     }
+//                     if player_position[1] == 3 {
+//                         if row3[2] == " " {
+//                             row3 = [row3[0], row3[1], current_player];
+//                             break;
+//                         }
+//                     }
+//                 }
 
                 
-            }
-            else if movement == 0 {
-                panic!();
-            }
-            else {
-                println!("That move is invalid please try again.");
-                continue; 
-            }
-            draw::draw_game_board(&row1, &row2, &row3, &player_position);
-            let winner = tests::has_someone_won(&row1, &row2, &row3);
-            println!("winner {}", winner)
-        }
-    }
+//             }
+//             else if movement == 0 {
+//                 panic!();
+//             }
+//             else {
+//                 println!("That move is invalid please try again.");
+//                 continue; 
+//             }
+//             draw::draw_game_board(&row1, &row2, &row3, &player_position);
+//             let winner = tests::has_someone_won(&row1, &row2, &row3);
+//             println!("winner {}", winner)
+//         }
+//     }
 
-    // draw::draw_game_board(&row1, &row2, &row3, &player_position);
-    // let winner = tests::has_someone_won(&row1, &row2, &row3);
-    // println!("winner {}", winner)
-}
+//     // draw::draw_game_board(&row1, &row2, &row3, &player_position);
+//     // let winner = tests::has_someone_won(&row1, &row2, &row3);
+//     // println!("winner {}", winner)
+// }
 
-fn move_player(movement: u32, mut player_position: [i32; 2]) -> [i32; 2]{
-    if movement == 4 {
-        if player_position[1] == 1 {
-            println!("That move is invalid please try again.");
-        }
-        else {
-            player_position = [player_position[0] , player_position[1] - 1];
-        }
-    }
-    else if movement == 6 {
-        if player_position[1] == 3 {
-            println!("That move is invalid please try again.");
-        }
-        else {
-            player_position = [player_position[0] , player_position[1] + 1];
-        }
-    }
-    else if movement == 8 {
-        if player_position[0] == 1 {
-            println!("That move is invalid please try again.");
-        }
-        else {
-            player_position = [player_position[0] - 1 , player_position[1]];
-        }
-    }
-    else if movement == 2 {
-        if player_position[0] == 3 {
-            println!("That move is invalid please try again.");
-        }
-        else {
-            player_position = [player_position[0] + 1, player_position[1]];
-        }
-    }
-    player_position
-}
+// fn move_player(movement: u32, mut player_position: [i32; 2]) -> [i32; 2]{
+//     if movement == 4 {
+//         if player_position[1] == 1 {
+//             println!("That move is invalid please try again.");
+//         }
+//         else {
+//             player_position = [player_position[0] , player_position[1] - 1];
+//         }
+//     }
+//     else if movement == 6 {
+//         if player_position[1] == 3 {
+//             println!("That move is invalid please try again.");
+//         }
+//         else {
+//             player_position = [player_position[0] , player_position[1] + 1];
+//         }
+//     }
+//     else if movement == 8 {
+//         if player_position[0] == 1 {
+//             println!("That move is invalid please try again.");
+//         }
+//         else {
+//             player_position = [player_position[0] - 1 , player_position[1]];
+//         }
+//     }
+//     else if movement == 2 {
+//         if player_position[0] == 3 {
+//             println!("That move is invalid please try again.");
+//         }
+//         else {
+//             player_position = [player_position[0] + 1, player_position[1]];
+//         }
+//     }
+//     player_position
+// }
--- src/tests.rs
+++ src/tests.rs
@@ -1,52 +1,52 @@
-pub fn has_someone_won(
-    row1: &[&str; 3],
-    row2: &[&str; 3],
-    row3: &[&str; 3]
-    ) -> String {
+// pub fn has_someone_won(
+//     row1: &[&str; 3],
+//     row2: &[&str; 3],
+//     row3: &[&str; 3]
+//     ) -> String {
     
-    let player = ["x", "0"] ;
-    let mut winner = "none".to_string();
-    for &player in player.iter() {
-        if (row1[0] == player) && (row1[1] == player) && (row1[2] == player){
-            mention_the_winner(player.to_string());
-            winner = player.to_string();
-        }
-        else if (row2[0] == player) && (row2[1] == player) && (row2[2] == player){
-            mention_the_winner(player.to_string());
-            winner = player.to_string();
-        }
-        else if (row3[0] == player) && (row3[1] == player) && (row3[2] == player){
-            mention_the_winner(player.to_string());
-            winner = player.to_string();
-        }
+//     let player = ["x", "0"] ;
+//     let mut winner = "none".to_string();
+//     for &player in player.iter() {
+//         if (row1[0] == player) && (row1[1] == player) && (row1[2] == player){
+//             mention_the_winner(player.to_string());
+//             winner = player.to_string();
+//         }
+//         else if (row2[0] == player) && (row2[1] == player) && (row2[2] == player){
+//             mention_the_winner(player.to_string());
+//             winner = player.to_string();
+//         }
+//         else if (row3[0] == player) && (row3[1] == player) && (row3[2] == player){
+//             mention_the_winner(player.to_string());
+//             winner = player.to_string();
+//         }
 
 
 
-        else if (row1[0] == player) && (row2[0] == player) && (row3[0] == player){
-            mention_the_winner(player.to_string());
-            winner = player.to_string();
-        }
-        else if (row1[1] == player) && (row2[1] == player) && (row3[1] == player){
-            mention_the_winner(player.to_string());
-            winner = player.to_string();
-        }
-        else if (row1[2] == player) && (row2[2] == player) && (row3[2] == player){
-            mention_the_winner(player.to_string());
-            winner = player.to_string();
-        }
+//         else if (row1[0] == player) && (row2[0] == player) && (row3[0] == player){
+//             mention_the_winner(player.to_string());
+//             winner = player.to_string();
+//         }
+//         else if (row1[1] == player) && (row2[1] == player) && (row3[1] == player){
+//             mention_the_winner(player.to_string());
+//             winner = player.to_string();
+//         }
+//         else if (row1[2] == player) && (row2[2] == player) && (row3[2] == player){
+//             mention_the_winner(player.to_string());
+//             winner = player.to_string();
+//         }
 
 
-        else if (row1[0] == player) && (row2[1] == player) && (row3[2] == player){
-            mention_the_winner(player.to_string());
-            winner = player.to_string();
-        }
-        else if (row3[0] == player) && (row2[1] == player) && (row1[2] == player){
-            mention_the_winner(player.to_string());
-            winner = player.to_string();
-        }
-    }
-    fn mention_the_winner(winner: String) {
-        println!("The winner is {}", winner);
-    }
-    winner.to_string()
-}
+//         else if (row1[0] == player) && (row2[1] == player) && (row3[2] == player){
+//             mention_the_winner(player.to_string());
+//             winner = player.to_string();
+//         }
+//         else if (row3[0] == player) && (row2[1] == player) && (row1[2] == player){
+//             mention_the_winner(player.to_string());
+//             winner = player.to_string();
+//         }
+//     }
+//     fn mention_the_winner(winner: String) {
+//         println!("The winner is {}", winner);
+//     }
+//     winner.to_string()
+// }
