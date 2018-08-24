6eac388b0a56c25ad245993a7ee266e426d8f7b5
Ethan Brierley <ethanboxx@gmail.com>
2018-08-24 11:06:13 +0100

    cross

--- src/tests.rs
+++ src/tests.rs
@@ -4,12 +4,13 @@ use TileStatus;
 use Winner;
 
 pub fn has_someone_won(current_player: Players, game_board: GameBoard) -> Winner {
-    let player_being_tested = [Players::Nought, Players::Cross];
     match current_player {
         Players::Nought => nought(game_board),
-        Players::Cross => Winner::None,
+        Players::Cross => cross(game_board),
     }
 }
+
+
 
 fn nought(game_board: GameBoard) -> Winner {
     match nought_first_row(game_board) {
@@ -145,55 +146,134 @@ fn nought_diag_two(game_board: GameBoard) -> Winner{
 
 
 
-// pub fn has_someone_won(
-//     row1: &[&str; 3],
-//     row2: &[&str; 3],
-//     row3: &[&str; 3]
-//     ) -> String {
-    
-//     let player = ["x", "0"] ;
-//     let mut winner = "none".to_string();
-//     for &player in player.iter() {
-//         if (row1[0] == player) && (row1[1] == player) && (row1[2] == player){
-//             mention_the_winner(player.to_string());
-//             winner = player.to_string();
-//         }
-//         else if (row2[0] == player) && (row2[1] == player) && (row2[2] == player){
-//             mention_the_winner(player.to_string());
-//             winner = player.to_string();
-//         }
-//         else if (row3[0] == player) && (row3[1] == player) && (row3[2] == player){
-//             mention_the_winner(player.to_string());
-//             winner = player.to_string();
-//         }
+fn cross(game_board: GameBoard) -> Winner {
+    match cross_first_row(game_board) {
+        Winner::Cross => Winner::Cross,
+        _ => match cross_second_row(game_board) {
+            Winner::Cross => Winner::Cross,
+            _ => match cross_third_row(game_board) {
+                Winner::Cross => Winner::Cross,
+                _ => match cross_first_col(game_board) {
+                    Winner::Cross => Winner::Cross,
+                    _ => match cross_second_col(game_board) {
+                        Winner::Cross => Winner::Cross,
+                        _ => match cross_third_col(game_board) {
+                            Winner::Cross => Winner::Cross,
+                            _ => match cross_diag_one(game_board) {
+                                Winner::Cross => Winner::Cross,
+                                _ => match cross_diag_two(game_board) {
+                                    Winner::Cross => Winner::Cross,
+                                    _ => Winner::None,
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
+fn cross_first_row(game_board: GameBoard) -> Winner{
+        match game_board.row_one[0] {
+        TileStatus::Cross(_cursor) => match game_board.row_one[1] {
+            TileStatus::Cross(_cursor) => match game_board.row_one[2] {
+                TileStatus::Cross(_cursor) => Winner::Cross,
+                _ => Winner::None
+            },
+            _ => Winner::None,
+        },
+        _ => Winner::None,
+    }
+}
 
+fn cross_second_row(game_board: GameBoard) -> Winner{
+        match game_board.row_two[0] {
+        TileStatus::Cross(_cursor) => match game_board.row_two[1] {
+            TileStatus::Cross(_cursor) => match game_board.row_two[2] {
+                TileStatus::Cross(_cursor) => Winner::Cross,
+                _ => Winner::None
+            },
+            _ => Winner::None,
+        },
+        _ => Winner::None,
+    }
+}
 
-//         else if (row1[0] == player) && (row2[0] == player) && (row3[0] == player){
-//             mention_the_winner(player.to_string());
-//             winner = player.to_string();
-//         }
-//         else if (row1[1] == player) && (row2[1] == player) && (row3[1] == player){
-//             mention_the_winner(player.to_string());
-//             winner = player.to_string();
-//         }
-//         else if (row1[2] == player) && (row2[2] == player) && (row3[2] == player){
-//             mention_the_winner(player.to_string());
-//             winner = player.to_string();
-//         }
+fn cross_third_row(game_board: GameBoard) -> Winner{
+        match game_board.row_three[0] {
+        TileStatus::Cross(_cursor) => match game_board.row_three[1] {
+            TileStatus::Cross(_cursor) => match game_board.row_three[2] {
+                TileStatus::Cross(_cursor) => Winner::Cross,
+                _ => Winner::None
+            },
+            _ => Winner::None,
+        },
+        _ => Winner::None,
+    }
+}
 
+fn cross_first_col(game_board: GameBoard) -> Winner{
+        match game_board.row_one[0] {
+        TileStatus::Cross(_cursor) => match game_board.row_two[0] {
+            TileStatus::Cross(_cursor) => match game_board.row_three[0] {
+                TileStatus::Cross(_cursor) => Winner::Cross,
+                _ => Winner::None
+            },
+            _ => Winner::None,
+        },
+        _ => Winner::None,
+    }
+}
 
-//         else if (row1[0] == player) && (row2[1] == player) && (row3[2] == player){
-//             mention_the_winner(player.to_string());
-//             winner = player.to_string();
-//         }
-//         else if (row3[0] == player) && (row2[1] == player) && (row1[2] == player){
-//             mention_the_winner(player.to_string());
-//             winner = player.to_string();
-//         }
-//     }
-//     fn mention_the_winner(winner: String) {
-//         println!("The winner is {}", winner);
-//     }
-//     winner.to_string()
-// }
+fn cross_second_col(game_board: GameBoard) -> Winner{
+        match game_board.row_one[1] {
+        TileStatus::Cross(_cursor) => match game_board.row_two[1] {
+            TileStatus::Cross(_cursor) => match game_board.row_three[1] {
+                TileStatus::Cross(_cursor) => Winner::Cross,
+                _ => Winner::None
+            },
+            _ => Winner::None,
+        },
+        _ => Winner::None,
+    }
+}
+
+fn cross_third_col(game_board: GameBoard) -> Winner{
+        match game_board.row_one[2] {
+        TileStatus::Cross(_cursor) => match game_board.row_two[2] {
+            TileStatus::Cross(_cursor) => match game_board.row_three[2] {
+                TileStatus::Cross(_cursor) => Winner::Cross,
+                _ => Winner::None
+            },
+            _ => Winner::None,
+        },
+        _ => Winner::None,
+    }
+}
+
+fn cross_diag_one(game_board: GameBoard) -> Winner{
+        match game_board.row_one[0] {
+        TileStatus::Cross(_cursor) => match game_board.row_two[1] {
+            TileStatus::Cross(_cursor) => match game_board.row_three[2] {
+                TileStatus::Cross(_cursor) => Winner::Cross,
+                _ => Winner::None
+            },
+            _ => Winner::None,
+        },
+        _ => Winner::None,
+    }
+}
+
+fn cross_diag_two(game_board: GameBoard) -> Winner{
+        match game_board.row_one[2] {
+        TileStatus::Cross(_cursor) => match game_board.row_two[1] {
+            TileStatus::Cross(_cursor) => match game_board.row_three[0] {
+                TileStatus::Cross(_cursor) => Winner::Cross,
+                _ => Winner::None
+            },
+            _ => Winner::None,
+        },
+        _ => Winner::None,
+    }
+}
