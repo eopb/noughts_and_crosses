895c193c262246ea0df06761bcc5fd6c660dbfbf
Ethan Brierley <ethanboxx@gmail.com>
2018-08-24 22:05:02 +0100

    bug fix

--- src/movement/mod.rs
+++ src/movement/mod.rs
@@ -72,7 +72,7 @@ fn place_player(game_board: GameBoard, current_player: Players) -> GameBoard {
             TileStatus::Cursor => match current_player {
                 Players::Cross | Players::Nought => GameBoard {
                     row_one: [
-                        TileStatus::Cross(Cursor::None),
+                        TileStatus::Cross(Cursor::True),
                         game_board.row_one[1],
                         game_board.row_one[2],
                     ],
@@ -88,7 +88,7 @@ fn place_player(game_board: GameBoard, current_player: Players) -> GameBoard {
                 Players::Cross | Players::Nought => GameBoard {
                     row_one: [
                         game_board.row_one[0],
-                        TileStatus::Cross(Cursor::None),
+                        TileStatus::Cross(Cursor::True),
                         game_board.row_one[2],
                     ],
                     ..game_board
@@ -104,7 +104,7 @@ fn place_player(game_board: GameBoard, current_player: Players) -> GameBoard {
                     row_one: [
                         game_board.row_one[0],
                         game_board.row_one[1],
-                        TileStatus::Cross(Cursor::None),
+                        TileStatus::Cross(Cursor::True),
                     ],
                     ..game_board
                 },
@@ -117,7 +117,7 @@ fn place_player(game_board: GameBoard, current_player: Players) -> GameBoard {
             TileStatus::Cursor => match current_player {
                 Players::Cross | Players::Nought => GameBoard {
                     row_two: [
-                        TileStatus::Cross(Cursor::None),
+                        TileStatus::Cross(Cursor::True),
                         game_board.row_two[1],
                         game_board.row_two[2],
                     ],
@@ -133,7 +133,7 @@ fn place_player(game_board: GameBoard, current_player: Players) -> GameBoard {
                 Players::Cross | Players::Nought => GameBoard {
                     row_two: [
                         game_board.row_two[0],
-                        TileStatus::Cross(Cursor::None),
+                        TileStatus::Cross(Cursor::True),
                         game_board.row_two[2],
                     ],
                     ..game_board
@@ -149,7 +149,7 @@ fn place_player(game_board: GameBoard, current_player: Players) -> GameBoard {
                     row_two: [
                         game_board.row_two[0],
                         game_board.row_two[1],
-                        TileStatus::Cross(Cursor::None),
+                        TileStatus::Cross(Cursor::True),
                     ],
                     ..game_board
                 },
@@ -162,7 +162,7 @@ fn place_player(game_board: GameBoard, current_player: Players) -> GameBoard {
             TileStatus::Cursor => match current_player {
                 Players::Cross | Players::Nought => GameBoard {
                     row_three: [
-                        TileStatus::Cross(Cursor::None),
+                        TileStatus::Cross(Cursor::True),
                         game_board.row_three[1],
                         game_board.row_three[2],
                     ],
@@ -178,7 +178,7 @@ fn place_player(game_board: GameBoard, current_player: Players) -> GameBoard {
                 Players::Cross | Players::Nought => GameBoard {
                     row_three: [
                         game_board.row_three[0],
-                        TileStatus::Cross(Cursor::None),
+                        TileStatus::Cross(Cursor::True),
                         game_board.row_three[2],
                     ],
                     ..game_board
@@ -194,7 +194,7 @@ fn place_player(game_board: GameBoard, current_player: Players) -> GameBoard {
                     row_three: [
                         game_board.row_three[0],
                         game_board.row_three[1],
-                        TileStatus::Cross(Cursor::None),
+                        TileStatus::Cross(Cursor::True),
                     ],
                     ..game_board
                 },
@@ -204,9 +204,7 @@ fn place_player(game_board: GameBoard, current_player: Players) -> GameBoard {
         }
     } else {
         game_board
-    };
-    println!("here true");
-    game_board
+    }
 }
 
 pub fn is_cursor(tile: TileStatus) -> bool {
