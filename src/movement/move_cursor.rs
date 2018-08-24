26e7613a94e98c8184b776aa8ce55e98c59e87db
Ethan Brierley <ethanboxx@gmail.com>
2018-08-24 17:31:57 +0100

    refactor

---
+++ src/movement/move_cursor.rs
@@ -0,0 +1,153 @@
+use GameBoard;
+use TileStatus;
+use Cursor;
+use movement::Movement;
+
+
+pub fn move_cursor(game_board: GameBoard, inputed_movement: Movement) -> GameBoard {
+    if is_cursor(game_board.row_one[0]){
+        return match inputed_movement {
+            Movement::Right => GameBoard {
+                row_one: [
+                    remove_cursor(game_board.row_one[0]),
+                    add_cursor(game_board.row_one[1]),
+                    game_board.row_one[2]
+                ],
+                ..game_board
+            },
+            Movement::Down => GameBoard {
+                row_one: [
+                    remove_cursor(game_board.row_one[0]),
+                    game_board.row_one[1],
+                    game_board.row_one[2]
+                ],
+                row_two: [
+                    add_cursor(game_board.row_two[0]),
+                    game_board.row_two[1],
+                    game_board.row_two[2]
+                ],
+                ..game_board
+            },
+            _ => {
+                println!("This can not be done");
+                game_board
+            },
+        };
+    }
+    else if is_cursor(game_board.row_one[1]){
+        return match inputed_movement {
+            Movement::Right => GameBoard {
+                row_one: [
+                    game_board.row_one[0],
+                    remove_cursor(game_board.row_one[1]),
+                    add_cursor(game_board.row_one[2])
+                ],
+                ..game_board
+            },
+            Movement::Left => GameBoard {
+                row_one: [
+                    add_cursor(game_board.row_one[0]),
+                    remove_cursor(game_board.row_one[1]),
+                    game_board.row_one[2]
+                ],
+                ..game_board
+            },
+            Movement::Down => GameBoard {
+                row_one: [
+                    game_board.row_one[0],
+                    remove_cursor(game_board.row_one[1]),
+                    game_board.row_one[2]
+                ],
+                row_two: [
+                    game_board.row_two[0],
+                    add_cursor(game_board.row_two[1]),
+                    game_board.row_two[2]
+                ],
+                ..game_board
+            },
+            _ => {
+                println!("This can not be done");
+                game_board
+            },
+        };
+    }
+    else if is_cursor(game_board.row_one[2]){
+        return match inputed_movement {
+            Movement::Left => GameBoard {
+                row_one: [
+                    game_board.row_one[0],
+                    add_cursor(game_board.row_one[1]),
+                    remove_cursor(game_board.row_one[2])
+                ],
+                ..game_board
+            },
+            Movement::Down => GameBoard {
+                row_one: [
+                    game_board.row_one[0],
+                    game_board.row_one[1],
+                    remove_cursor(game_board.row_one[2])
+                ],
+                row_two: [
+                    game_board.row_two[0],
+                    game_board.row_two[1],
+                    add_cursor(game_board.row_two[2])
+                ],
+                ..game_board
+            },
+            _ => {
+                println!("This can not be done");
+                game_board
+            },
+        };
+    }
+    else {
+        println!("here");
+        panic!();
+    }
+
+} 
+
+fn remove_cursor(tile: TileStatus) -> TileStatus {
+    match tile {
+        TileStatus::Cross(cursor) => match cursor {
+                Cursor::True => TileStatus::Cross(Cursor::None),
+                Cursor::None => TileStatus::Cross(Cursor::None),
+            },
+        TileStatus::Nought(cursor) => match cursor {
+                Cursor::True => TileStatus::Nought(Cursor::None),
+                Cursor::None => TileStatus::Nought(Cursor::None),
+            },
+        TileStatus::Cursor => TileStatus::None,
+        TileStatus::None => TileStatus::None,
+    }
+}
+
+fn add_cursor(tile: TileStatus) -> TileStatus {
+    match tile {
+        TileStatus::Cross(cursor) => match cursor {
+                Cursor::True => TileStatus::Cross(Cursor::True),
+                Cursor::None => TileStatus::Cross(Cursor::True),
+            },
+        TileStatus::Nought(cursor) => match cursor {
+                Cursor::True => TileStatus::Nought(Cursor::True),
+                Cursor::None => TileStatus::Nought(Cursor::True),
+            },
+        TileStatus::Cursor => TileStatus::Cursor,
+        TileStatus::None => TileStatus::Cursor,
+    }
+}
+
+fn is_cursor(tile: TileStatus) -> bool {
+    match tile {
+        TileStatus::Cursor => true,
+        TileStatus::Nought(cursor) => match cursor {
+            Cursor::True => true,
+            Cursor::None => false,
+            },
+        TileStatus::Cross(cursor) => match cursor {
+            Cursor::True => true,
+            Cursor::None => false,
+            },
+        TileStatus::None => false
+    }
+}
