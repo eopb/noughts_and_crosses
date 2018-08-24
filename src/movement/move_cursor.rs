68ea2aaa585542007e9bcef61c4c0c0454dc0dce
Ethan Brierley <ethanboxx@gmail.com>
2018-08-24 18:32:48 +0100

    game_board.row_three[1]

--- src/movement/move_cursor.rs
+++ src/movement/move_cursor.rs
@@ -263,6 +263,43 @@ pub fn move_cursor(game_board: GameBoard, inputed_movement: Movement) -> GameBoa
             },
         };
     }
+    else if is_cursor(game_board.row_three[1]){
+        return match inputed_movement {
+            Movement::Right => GameBoard {
+                row_three: [
+                    game_board.row_three[0],
+                    remove_cursor(game_board.row_three[1]),
+                    add_cursor(game_board.row_three[2])
+                ],
+                ..game_board
+            },
+            Movement::Left => GameBoard {
+                row_three: [
+                    add_cursor(game_board.row_three[0]),
+                    remove_cursor(game_board.row_three[1]),
+                    game_board.row_three[2]
+                ],
+                ..game_board
+            },
+            Movement::Up => GameBoard {
+                row_three: [
+                    game_board.row_three[0],
+                    remove_cursor(game_board.row_three[1]),
+                    game_board.row_three[2]
+                ],
+                row_two: [
+                    game_board.row_two[0],
+                    add_cursor(game_board.row_two[1]),
+                    game_board.row_two[2]
+                ],
+                ..game_board
+            },
+            _ => {
+                println!("This can not be done");
+                game_board
+            },
+        };
+    }
     else {
         println!("here");
         panic!();
