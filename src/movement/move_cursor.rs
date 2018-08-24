4e75ee982b3f07ed3598f35ed8153552888e4a19
Ethan Brierley <ethanboxx@gmail.com>
2018-08-24 18:12:20 +0100

    game_board.row_two[1]

--- src/movement/move_cursor.rs
+++ src/movement/move_cursor.rs
@@ -142,6 +142,56 @@ pub fn move_cursor(game_board: GameBoard, inputed_movement: Movement) -> GameBoa
             },
         };
     }
+    else if is_cursor(game_board.row_two[1]){
+        return match inputed_movement {
+            Movement::Right => GameBoard {
+                row_two: [
+                    game_board.row_one[0],
+                    remove_cursor(game_board.row_one[1]),
+                    add_cursor(game_board.row_one[2])
+                ],
+                ..game_board
+            },
+            Movement::Left => GameBoard {
+                row_two: [
+                    add_cursor(game_board.row_one[0]),
+                    remove_cursor(game_board.row_one[1]),
+                    game_board.row_one[2]
+                ],
+                ..game_board
+            },
+            Movement::Down => GameBoard {
+                row_two: [
+                    game_board.row_one[0],
+                    remove_cursor(game_board.row_one[1]),
+                    game_board.row_one[2]
+                ],
+                row_three: [
+                    game_board.row_two[0],
+                    add_cursor(game_board.row_two[1]),
+                    game_board.row_two[2]
+                ],
+                ..game_board
+            },
+            Movement::Up => GameBoard {
+                row_one: [
+                    game_board.row_one[0],
+                    add_cursor(game_board.row_one[1]),
+                    game_board.row_one[2]
+                ],
+                row_two: [
+                    game_board.row_two[0],
+                    remove_cursor(game_board.row_two[1]),
+                    game_board.row_two[2]
+                ],
+                ..game_board
+            },
+            _ => {
+                println!("This can not be done");
+                game_board
+            },
+        };
+    }
     else {
         println!("here");
         panic!();
