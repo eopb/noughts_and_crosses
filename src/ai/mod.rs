7326957dd5062f9606b6f4bb46b7850afe1f5c11
Ethan Brierley <ethanboxx@gmail.com>
2018-08-25 16:26:35 +0100

    move random_placement to func to use later

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -7,6 +7,36 @@ use TileStatus;
 
 pub fn process_ai(game_board: GameBoard) -> GameBoard {
     let player_to_place = Players::Nought;
+    random_placement(game_board, player_to_place)
+}
+
+fn no_player(tile: TileStatus) -> bool {
+    match tile {
+        TileStatus::Cursor => true,
+        TileStatus::Nought(_cursor) => false,
+        TileStatus::Cross(_cursor) => false,
+        TileStatus::None => true,
+    }
+}
+
+fn place_player(tile: TileStatus, player_to_place: Players) -> TileStatus {
+    match tile {
+        TileStatus::Nought(_cursor) | TileStatus::Cross(_cursor) => {
+            println!("Error going to painc");
+            panic!();
+        }
+        TileStatus::Cursor => match player_to_place {
+            Players::Cross => TileStatus::Cross(Cursor::True),
+            Players::Nought => TileStatus::Nought(Cursor::True),
+        },
+        TileStatus::None => match player_to_place {
+            Players::Cross => TileStatus::Cross(Cursor::None),
+            Players::Nought => TileStatus::Nought(Cursor::None),
+        },
+    }
+}
+
+fn random_placement(game_board: GameBoard, player_to_place: Players) -> GameBoard {
     loop {
         let random_tile = rand::thread_rng().gen_range(1, 10);
         println!("Trying {}", random_tile);
@@ -102,31 +132,5 @@ pub fn process_ai(game_board: GameBoard) -> GameBoard {
         } else {
             continue;
         };
-    }
-}
-
-fn no_player(tile: TileStatus) -> bool {
-    match tile {
-        TileStatus::Cursor => true,
-        TileStatus::Nought(_cursor) => false,
-        TileStatus::Cross(_cursor) => false,
-        TileStatus::None => true,
-    }
-}
-
-fn place_player(tile: TileStatus, player_to_place: Players) -> TileStatus {
-    match tile {
-        TileStatus::Nought(_cursor) | TileStatus::Cross(_cursor) => {
-            println!("Error going to painc");
-            panic!();
-        }
-        TileStatus::Cursor => match player_to_place {
-            Players::Cross => TileStatus::Cross(Cursor::True),
-            Players::Nought => TileStatus::Nought(Cursor::True),
-        },
-        TileStatus::None => match player_to_place {
-            Players::Cross => TileStatus::Cross(Cursor::None),
-            Players::Nought => TileStatus::Nought(Cursor::None),
-        },
     }
 }
