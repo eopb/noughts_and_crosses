8cc19e9b23357fc3a2ace3a791fddae87a358be3
Ethan Brierley <ethanboxx@gmail.com>
2018-08-25 15:08:46 +0100

    finish nice random ai

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -6,7 +6,7 @@ use Players;
 use TileStatus;
 
 pub fn process_ai(game_board: GameBoard) -> GameBoard {
-    let player_to_place = Players::Cross;
+    let player_to_place = Players::Nought;
     loop {
         let random_tile = rand::thread_rng().gen_range(1, 10);
         println!("{}", random_tile);
@@ -36,6 +36,66 @@ pub fn process_ai(game_board: GameBoard) -> GameBoard {
                     game_board.row_one[0],
                     game_board.row_one[1],
                     place_player(game_board.row_one[2], player_to_place),
+                ],
+                ..game_board
+            };
+        }
+        if no_player(game_board.row_two[0]) && (random_tile == 4) {
+            return GameBoard {
+                row_two: [
+                    place_player(game_board.row_two[0], player_to_place),
+                    game_board.row_two[1],
+                    game_board.row_two[2],
+                ],
+                ..game_board
+            };
+        }
+        if no_player(game_board.row_two[1]) && (random_tile == 5) {
+            return GameBoard {
+                row_two: [
+                    game_board.row_two[0],
+                    place_player(game_board.row_two[1], player_to_place),
+                    game_board.row_two[2],
+                ],
+                ..game_board
+            };
+        }
+        if no_player(game_board.row_two[2]) && (random_tile == 6) {
+            return GameBoard {
+                row_two: [
+                    game_board.row_two[0],
+                    game_board.row_two[1],
+                    place_player(game_board.row_two[2], player_to_place),
+                ],
+                ..game_board
+            };
+        }
+        if no_player(game_board.row_three[0]) && (random_tile == 7) {
+            return GameBoard {
+                row_three: [
+                    place_player(game_board.row_three[0], player_to_place),
+                    game_board.row_three[1],
+                    game_board.row_three[2],
+                ],
+                ..game_board
+            };
+        }
+        if no_player(game_board.row_three[1]) && (random_tile == 8) {
+            return GameBoard {
+                row_three: [
+                    game_board.row_three[0],
+                    place_player(game_board.row_three[1], player_to_place),
+                    game_board.row_three[2],
+                ],
+                ..game_board
+            };
+        }
+        if no_player(game_board.row_three[2]) && (random_tile == 9) {
+            return GameBoard {
+                row_three: [
+                    game_board.row_three[0],
+                    game_board.row_three[1],
+                    place_player(game_board.row_three[2], player_to_place),
                 ],
                 ..game_board
             };
