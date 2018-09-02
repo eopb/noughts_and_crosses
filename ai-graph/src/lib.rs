f45405d86e3b53b1c7fd383841f8a6e0b6cb1318
Ethan Brierley <ethanboxx@gmail.com>
2018-09-02 20:17:59 +0100

    Fix for clippy

--- ai-graph/src/lib.rs
+++ ai-graph/src/lib.rs
@@ -1,9 +1,1 @@
-mod ai_graph {
-    pub fn run(inputs: &[f64]) -> Vec<&f64> {
-        let mut output = Vec::new();
-        for input in inputs {
-            output.push(input);
-        }
-        output
-    }
-}
+
