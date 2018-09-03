8a366532d5ebb02138dbf6271a57650d167a02e3
Ethan Brierley <ethanboxx@gmail.com>
2018-09-03 15:27:43 +0100

    test

--- ai-graph/src/new_genes.rs
+++ ai-graph/src/new_genes.rs
@@ -8,4 +8,10 @@ impl Gene {
             NodeDna: vec![vec![MutationNode::Add; 9]; 2],
         }
     }
+    pub fn new_random_gene() -> Gene {
+        Gene {
+            LineDna: vec![vec![vec![MutationLine::None; 9]; 9]; 2],
+            NodeDna: vec![vec![MutationNode::Add; 9]; 2],
+        }
+    }
 }
