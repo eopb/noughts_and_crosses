e2dad13215005b8af1579996e3748954218d08bf
Ethan Brierley <ethanboxx@gmail.com>
2018-09-03 21:42:50 +0100

    MutationNodeStorage

--- ai-graph/src/lib.rs
+++ ai-graph/src/lib.rs
@@ -17,6 +17,11 @@ enum MutationNode {
     Divide,
     Subtract,
 }
+#[derive(Debug)]
+struct MutationNodeStorage {
+    node_type: MutationNode,
+    stored_data: Option<f64>,
+}
 #[derive(Clone, Debug)]
 pub struct Gene {
     line_dna: Vec<Vec<Vec<MutationLine>>>,
@@ -34,6 +39,48 @@ mod ai_graph {
 }
 impl Gene {
     pub fn output(self, input: Vec<i32>) -> Vec<f64> {
-        vec![0.0; 9]
+        let mut output = vec![0.0; 9];
+        let node_values = node_value_calc(&self.node_dna);
+        print!("node values {:#?}", node_values);
+        if !self.validate(input.to_vec()) {
+            panic!("Gene is not valid")
+        }
+        for node_tree in &self.line_dna[1] {
+            for line in node_tree {}
+        }
+        output[2] = 2.34;
+        output[7] = 3.52;
+        output
+    }
+    fn validate(&self, input: Vec<i32>) -> bool {
+        for value in &self.line_dna[0] {
+            if value.len() == self.node_dna[0].len() {
+                continue;
+            }
+            return false;
+        }
+        true
+    }
+}
+
+fn node_value_calc(node_dna: &Vec<Vec<MutationNode>>) -> Vec<Vec<MutationNodeStorage>> {
+    let mut output = Vec::new();
+    for row in node_dna {
+        output.push(node_value_calc_row(&row));
+    }
+    output
+}
+fn node_value_calc_row(row: &Vec<MutationNode>) -> Vec<MutationNodeStorage> {
+    let mut output = Vec::new();
+    for node in row {
+        output.push(convert_mut_node_to_mut_node_store(&node));
+    }
+    output
+}
+
+fn convert_mut_node_to_mut_node_store(node: &MutationNode) -> MutationNodeStorage {
+    MutationNodeStorage {
+        node_type: *node,
+        stored_data: None,
     }
 }
