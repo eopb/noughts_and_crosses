42379c471edf8f32315b736459cb4c12b47011ea
Ethan Brierley <ethanboxx@gmail.com>
2018-09-06 10:02:22 +0100

    work

--- noughts-and-crosses-lib/src/ai/smart_random/rating_board/process_rating_board.rs
+++ noughts-and-crosses-lib/src/ai/smart_random/rating_board/process_rating_board.rs
@@ -5,108 +5,145 @@ use ai::smart_random::RatingBoard;
 use GameBoard;
 use Players;
 use IS_DEBUG;
-
+//TODO Some and none for full boards. Stop the panic.
 impl GameBoard {
-    //TODO Check if there is no player in the tile before placing
-    pub fn place_largest_empty(self, inputs: &[f64], player_to_place: Players) -> Self {
+    pub fn place_largest_empty(self, inputs: &[f64], player_to_place: Players) -> Option<Self> {
+        let mut option_inputs = Vec::new();
+        for input in inputs {
+            option_inputs.push(Some(input))
+        }
+        // println!("inputs {:#?}", option_inputs);
         loop {
-            let random_tile = rand::thread_rng().gen_range(1, 10);
-            if IS_DEBUG {
-                println!("Trying2 {}", random_tile);
-            };
-            if highest_rating_ai(inputs[0], &inputs) && (random_tile == 1) {
-                return Self {
-                    row_one: [
-                        self.row_one[0].place_player(player_to_place),
-                        self.row_one[1],
-                        self.row_one[2],
-                    ],
-                    ..self
-                };
+            // println!("Trying2 {}", random_tile);
+
+            if highest_rating_ai(option_inputs[0], &option_inputs) {
+                if self.row_one[0].no_player() {
+                    return Some(Self {
+                        row_one: [
+                            self.row_one[0].place_player(player_to_place),
+                            self.row_one[1],
+                            self.row_one[2],
+                        ],
+                        ..self
+                    });
+                } else {
+                    option_inputs[0] = None;
+                }
             }
-            if highest_rating_ai(inputs[1], &inputs) && (random_tile == 2) {
-                return Self {
-                    row_one: [
-                        self.row_one[0],
-                        self.row_one[1].place_player(player_to_place),
-                        self.row_one[2],
-                    ],
-                    ..self
-                };
+            if highest_rating_ai(option_inputs[1], &option_inputs) {
+                if self.row_one[1].no_player() {
+                    return Some(Self {
+                        row_one: [
+                            self.row_one[0],
+                            self.row_one[1].place_player(player_to_place),
+                            self.row_one[2],
+                        ],
+                        ..self
+                    });
+                } else {
+                    option_inputs[1] = None;
+                }
             }
-            if highest_rating_ai(inputs[2], &inputs) && (random_tile == 3) {
-                return Self {
-                    row_one: [
-                        self.row_one[0],
-                        self.row_one[1],
-                        self.row_one[2].place_player(player_to_place),
-                    ],
-                    ..self
-                };
+            if highest_rating_ai(option_inputs[2], &option_inputs) {
+                if self.row_one[2].no_player() {
+                    return Some(Self {
+                        row_one: [
+                            self.row_one[0],
+                            self.row_one[1],
+                            self.row_one[2].place_player(player_to_place),
+                        ],
+                        ..self
+                    });
+                } else {
+                    option_inputs[2] = None;
+                }
             }
-            if highest_rating_ai(inputs[3], &inputs) && (random_tile == 4) {
-                return Self {
-                    row_two: [
-                        self.row_two[0].place_player(player_to_place),
-                        self.row_two[1],
-                        self.row_two[2],
-                    ],
-                    ..self
-                };
+            if highest_rating_ai(option_inputs[3], &option_inputs) {
+                if self.row_two[0].no_player() {
+                    return Some(Self {
+                        row_two: [
+                            self.row_two[0].place_player(player_to_place),
+                            self.row_two[1],
+                            self.row_two[2],
+                        ],
+                        ..self
+                    });
+                } else {
+                    option_inputs[3] = None;
+                }
             }
-            if highest_rating_ai(inputs[4], &inputs) && (random_tile == 5) {
-                return Self {
-                    row_two: [
-                        self.row_two[0],
-                        self.row_two[1].place_player(player_to_place),
-                        self.row_two[2],
-                    ],
-                    ..self
-                };
+            if highest_rating_ai(option_inputs[4], &option_inputs) {
+                if self.row_two[1].no_player() {
+                    return Some(Self {
+                        row_two: [
+                            self.row_two[0],
+                            self.row_two[1].place_player(player_to_place),
+                            self.row_two[2],
+                        ],
+                        ..self
+                    });
+                } else {
+                    option_inputs[4] = None;
+                }
             }
-            if highest_rating_ai(inputs[5], &inputs) && (random_tile == 6) {
-                return Self {
-                    row_two: [
-                        self.row_two[0],
-                        self.row_two[1],
-                        self.row_two[2].place_player(player_to_place),
-                    ],
-                    ..self
-                };
+            if highest_rating_ai(option_inputs[5], &option_inputs) {
+                if self.row_two[2].no_player() {
+                    return Some(Self {
+                        row_two: [
+                            self.row_two[0],
+                            self.row_two[1],
+                            self.row_two[2].place_player(player_to_place),
+                        ],
+                        ..self
+                    });
+                } else {
+                    option_inputs[5] = None;
+                }
             }
-            if highest_rating_ai(inputs[6], &inputs) && (random_tile == 7) {
-                return Self {
-                    row_three: [
-                        self.row_three[0].place_player(player_to_place),
-                        self.row_three[1],
-                        self.row_three[2],
-                    ],
-                    ..self
-                };
+            if highest_rating_ai(option_inputs[6], &option_inputs) {
+                if self.row_three[0].no_player() {
+                    return Some(Self {
+                        row_three: [
+                            self.row_three[0].place_player(player_to_place),
+                            self.row_three[1],
+                            self.row_three[2],
+                        ],
+                        ..self
+                    });
+                } else {
+                    option_inputs[6] = None;
+                }
             }
-            if highest_rating_ai(inputs[7], &inputs) && (random_tile == 8) {
-                return Self {
-                    row_three: [
-                        self.row_three[0],
-                        self.row_three[1].place_player(player_to_place),
-                        self.row_three[2],
-                    ],
-                    ..self
-                };
+            if highest_rating_ai(option_inputs[7], &option_inputs) {
+                if self.row_three[1].no_player() {
+                    return Some(Self {
+                        row_three: [
+                            self.row_three[0],
+                            self.row_three[1].place_player(player_to_place),
+                            self.row_three[2],
+                        ],
+                        ..self
+                    });
+                } else {
+                    option_inputs[7] = None;
+                }
             }
-            if highest_rating_ai(inputs[8], &inputs) && (random_tile == 9) {
-                return Self {
-                    row_three: [
-                        self.row_three[0],
-                        self.row_three[1],
-                        self.row_three[2].place_player(player_to_place),
-                    ],
-                    ..self
-                };
+            if highest_rating_ai(option_inputs[8], &option_inputs) {
+                if self.row_three[2].no_player() {
+                    return Some(Self {
+                        row_three: [
+                            self.row_three[0],
+                            self.row_three[1],
+                            self.row_three[2].place_player(player_to_place),
+                        ],
+                        ..self
+                    });
+                } else {
+                    option_inputs[8] = None;
+                }
             }
             if self.is_board_full() {
-                println!("This should not be happening :(");
-                panic!();
+                return None;
             }
         }
     }
@@ -256,13 +293,34 @@ fn highest_rating(rating_being_tested: Option<f64>, rating_board: RatingBoard) -
         }
     }
 }
-fn highest_rating_ai(rating_being_tested: f64, ratings: &[f64]) -> bool {
+fn highest_rating_ai(rating_being_tested: Option<&f64>, ratings: &Vec<Option<&f64>>) -> bool {
     for rating in ratings {
-        if &rating_being_tested > rating {
-            continue;
-        } else {
-            return false;
-        }
+        match &rating_being_tested {
+            Option::Some(rating_being_tested) => {
+                match rating {
+                    Option::Some(rating) => {
+                        if &rating_being_tested > &rating {
+                            // println!("somewhat here5");
+                            continue;
+                        } else if &rating_being_tested == &rating {
+                            // println!("somewhat here3");
+                            continue;
+                        } else {
+                            // println!("somewhat here2");
+                            return false;
+                        }
+                    }
+                    Option::None => {
+                        continue;
+                    }
+                };
+            }
+            None => {
+                // println!("somewhat here");
+                return false;
+            }
+        };
     }
+    // println!("somewhat here4");
     true
 }
--- noughts-and-crosses-random-train/src/main.rs
+++ noughts-and-crosses-random-train/src/main.rs
@@ -5,19 +5,19 @@ extern crate ai_graph;
 extern crate noughts_and_crosses_lib;
 
 use ai_graph::Gene;
-use noughts_and_crosses_lib::{GameBoard, Players, Winner};
+use noughts_and_crosses_lib::{GameBoard, GameMode, Players, Winner};
 
 #[derive(Debug, Clone)]
 struct GeneStorage {
     gene: Gene,
-    score: u32,
+    score: f64,
 }
 
 fn main() {
     let mut scores = Vec::new();
-    for _x in 0..10 {
+    for _x in 0..5 {
         let gene_tested = Gene::new_random_gene();
-        for _x in 0..10 {
+        for _x in 0..30 {
             let mut game_board = GameBoard::empty_board();
             let mut score_values = Vec::new();
             loop {
@@ -38,14 +38,37 @@ fn main() {
                     }
                     Winner::None => (),
                 };
-                game_board = game_board.place_largest_empty(
+                game_board.draw_game_board(&GameMode::Spectate);
+                game_board = match game_board.place_largest_empty(
                     &gene_tested.clone().output(&[1, 0, 1, 1, 1, 0, 0, 1, 1]),
                     Players::Nought,
-                )
+                ) {
+                    Some(game_board) => game_board,
+                    None => {
+                        score_values.push(0);
+                        break;
+                    }
+                };
+                game_board.draw_game_board(&GameMode::Spectate);
+                match game_board.has_someone_won() {
+                    Winner::Nought => {
+                        score_values.push(3);
+                        break;
+                    }
+                    Winner::Cross => {
+                        panic!("error 1");
+                    }
+                    Winner::None => (),
+                };
             }
             scores.push(GeneStorage {
                 gene: gene_tested.clone(),
-                score: score_values.iter().sum(): u32 / score_values.len() as u32,
+                score: score_values
+                    .iter()
+                    .cloned()
+                    .map(|val| val as f64)
+                    .sum::<f64>() as f64
+                    / score_values.len() as f64,
             });
         }
     }
