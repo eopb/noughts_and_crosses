123b049df348345a3a7b7dbb4ea3c6bc0e71f398
Ethan Brierley <ethanboxx@gmail.com>
2018-09-13 19:20:10 +0100

    only when it works + debug

--- ai-graph/src/tests.rs
+++ ai-graph/src/tests.rs
@@ -11,10 +11,12 @@ fn random_gene_is_valid() {
 #[test]
 fn random_breed_is_valid() {
     let mut test_gene = Gene::new_random_gene();
+    println!("test_gene{:#?}", test_gene);
     let test_gene2 = Gene::new_random_gene();
+    println!("test_gene2{:#?}", test_gene2);
     test_gene = test_gene.breed(&test_gene2);
-    println!("{:#?}", test_gene);
-    if !test_gene.validate() {
+    println!("test_gene2 mut{:#?}", test_gene);
+    if test_gene.validate() {
         panic!("Gene is not valid")
     };
 }
