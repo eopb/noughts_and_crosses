52ddf940c5c1e19a832346c62f55775d257ef1fc
Ethan Brierley <ethanboxx@gmail.com>
2018-09-14 08:17:43 +0100

    new validate tools

--- ai-graph/src/validate.rs
+++ ai-graph/src/validate.rs
@@ -2,12 +2,12 @@ use crate::Gene;
 
 impl Gene {
     pub fn validate(&self) -> bool {
-        for value in &self.line_dna[0] {
-            if value.len() == self.node_dna[0].len() {
-                continue;
-            }
-            return false;
-        }
+        self.all_lines_per_row_equel() && self.all_output_avalible()
+    }
+    fn all_lines_per_row_equel(&self) -> bool {
+        true
+    }
+    fn all_output_avalible(&self) -> bool {
         true
     }
 }
